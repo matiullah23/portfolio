72bf37434fe389d1709002443b2e60b2482eec29
Matiullah Hasher <35428976+matiullah23@users.noreply.github.com>
2018-08-07 06:52:49 -0400

    added js, css, html, and images

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+  $('#slides').superslides({
+    animation: 'fade',
+    play: 5000,
+    pagination: false
+  });
+
+  var typed = new Typed(".typed", {
+    strings: ["Data Analyst.", "Software Developer.", "Student."],
+    typeSpeed: 60,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+  });
+
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      480: {
+        items: 2
+      },
+      768: {
+        items: 3,
+      },
+      938: {
+        items: 4
+      }
+    }
+  })
+
+  $('.chart').easyPieChart({
+    easing: 'easeInOut',
+    barColor: '#fff',
+    trackColor: false,
+    scaleColor: false,
+    lineWidth: 4,
+    size: 152,
+    onStep: function(from, to, percent) {
+      $(this.el).find('.percent').text(Math.round(percent));
+    }
+
+  });
+
+});
