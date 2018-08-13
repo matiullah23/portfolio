d30548a682594699ea85f711c4c9520a88bd80c6
matiullah23 <matiullah.hasher@gmail.com>
2018-08-13 19:57:54 -0400

    updating portfolio

--- js/script.js
+++ js/script.js
@@ -35,69 +35,39 @@ $(document).ready(function() {
     }
   })
 
-  $('.chart').easyPieChart({
-    easing: 'easeInOut',
-    barColor: '#fff',
-    trackColor: false,
-    scaleColor: false,
-    lineWidth: 4,
-    size: 152,
-    onStep: function(from, to, percent) {
-      $(this.el).find('.percent').text(Math.round(percent));
-    }
-
-  });
 
 });
 
-$(document).ready(function() {
 
-  $('#slides').superslides({
-    animation: 'fade',
-    play: 5000,
-    pagination: false
-  });
+var skillsTopOffset = $(".skillsSection").offset().top;
+var statsTopOffset = $(".statsSection").offset().top;
+var countUpFinished = false;
 
-  var typed = new Typed(".typed", {
-    strings: ["Data Analyst.", "Software Developer.", "Student."],
-    typeSpeed: 60,
-    loop: true,
-    startDelay: 1000,
-    showCursor: false
-  });
+$(window).scroll(function() {
+  if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
 
-  $('.owl-carousel').owlCarousel({
-    loop: true,
-    items: 4,
-    // margin: 10,
-    // nav: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      480: {
-        items: 2
-      },
-      768: {
-        items: 3,
-      },
-      938: {
-        items: 4
+    $('.chart').easyPieChart({
+      easing: 'easeInOut',
+      barColor: '#fff',
+      trackColor: false,
+      scaleColor: false,
+      lineWidth: 4,
+      size: 152,
+      onStep: function(from, to, percent) {
+        $(this.el).find('.percent').text(Math.round(percent));
       }
-    }
-  });
 
-  $('.chart').easyPieChart({
-    easing: 'easeInOut',
-    barColor: '#fff',
-    trackColor: false,
-    scaleColor: false,
-    lineWidth: 4,
-    size: 152,
-    onStep: function(from, to, percent) {
-      $(this.el).find('.percent').text(Math.round(percent));
-    }
+    });
+  }
 
-  });
+  if (!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+    $(".counter").each(function() {
+      var element = $(this);
+      var endVal = parseInt(element.text());
+
+      element.countup(endVal);
+    })
+    countUpFinished = true;
+  }
 
 });
